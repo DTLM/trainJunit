94f6aeb4231faf93b13c3c0f686b677cd4a381c6
Thiago Martins <thiagomartins@audax.mobi>
2023-09-06 16:58:36 -0300

    feat: primeiro teste

--- src/test/java/treinoJunit/TesteCliente.java
+++ src/test/java/treinoJunit/TesteCliente.java
@@ -6,6 +6,9 @@ import org.junit.jupiter.api.Test;
 
 import treinoJunit.main.modal.Cliente;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+
 public class TesteCliente {
 
 	public final Cliente CLIENTE = new Cliente("Thiago", 2000.0 , LocalDate.of(1999, 4, 22), "126.275.174-81");
@@ -13,5 +16,21 @@ public class TesteCliente {
 	@Test
 	public void testarObjetoCliente() {
 		
+	}
+	
+	/*
+	 * Teste para validar vetores
+	 * 
+	 * O primeiro teste da errado de proposito para mostrar a situação de erro
+	 * e o segundo é a forma esperada.
+	 */
+	@Test
+	void validarVetores() {
+		int [] a = {0,1,2};
+		int [] b = {3};
+		int [] ab = {0,1,2};
+		
+//		assertArrayEquals(a, b);
+		assertArrayEquals(a, ab);
 	}
 }
