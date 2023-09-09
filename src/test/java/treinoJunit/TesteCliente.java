3fccb1704211beb1e340035396636161c0dc2416
Thiago Martins <thiagolealmartins@gmail.com>
2023-09-09 01:37:55 -0300

    feat: novos testes de comparação e before e after

--- src/test/java/treinoJunit/TesteCliente.java
+++ src/test/java/treinoJunit/TesteCliente.java
@@ -2,6 +2,10 @@ package treinoJunit;
 
 import java.time.LocalDate;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import treinoJunit.main.modal.Cliente;
@@ -13,9 +17,14 @@ public class TesteCliente {
 
 	public final Cliente CLIENTE = new Cliente("Thiago", 2000.0 , LocalDate.of(1999, 4, 22), "126.275.174-81");
 	
+	/**
+	 * Teste de valores nulos e não nulos
+	 */
 	@Test
-	public void testarObjetoCliente() {
-		
+	void testarObjetoCliente() {
+		Cliente cliente = null;
+		assertNull(cliente);
+		assertNotNull(CLIENTE);
 	}
 	
 	/*
@@ -30,7 +39,44 @@ public class TesteCliente {
 		int [] b = {3};
 		int [] ab = {0,1,2};
 		
-//		assertArrayEquals(a, b);
+		assertNotEquals(a, b);
 		assertArrayEquals(a, ab);
+	}
+	
+	/**
+	 * teste de comparação entre dois numeros
+	 */
+	@Test
+	void testeBasico() {
+		double valor1 = 5.0;
+		double valor2 = 5.0;
+		assertEquals(valor1, valor2);
+	}
+	
+	
+	/**
+	 * Os seguintes testes são testes que ocorrem antes de tudo e 
+	 * cada vez que um teste for executado.
+	 * 
+	 * No caso dos que acontecem antes e depois de tudo, é necessario que sejam metodos staticos
+	 */
+	@BeforeAll
+	static void antesDeTodos() {
+		System.out.println("inicializando.");
+	}
+	
+	@BeforeEach
+	void antesDeCada() {
+		System.out.println("inicializando antes de cada");
+	}
+	
+	@AfterAll
+	static void depoissDetoos() {
+		System.out.println("finalizando.");
+	}
+	
+	@AfterEach
+	void depoisDeCada() {
+		System.out.println("finalizando antes de cada");
 	}
 }
